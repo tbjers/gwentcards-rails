86ffe6ab5d80a064fb046a05da35421c04d026f0
Torgny Bjers <torgny@xorcode.com>
2015-10-21 20:23:45 -0400

    Updated Swagger production URL

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,5 +1,5 @@
 GrapeSwaggerRails.options.app_name     = 'api.gwentcards.com'
 GrapeSwaggerRails.options.url          = '/v1/swagger_doc'
-GrapeSwaggerRails.options.app_url      = Rails.env.production? ? 'https://gwentcards.herokuapp.com' : 'http://localhost:3000'
+GrapeSwaggerRails.options.app_url      = Rails.env.production? ? 'https://gwentcards.rpgcamp.com' : 'http://localhost:3000'
 GrapeSwaggerRails.options.api_key_name = 'access_token'
 GrapeSwaggerRails.options.api_key_type = 'query'

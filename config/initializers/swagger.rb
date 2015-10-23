fc29c8fc722c0c18e1b4c6dc4f084398ec5b6903
Torgny Bjers <torgny@xorcode.com>
2015-10-22 20:38:59 -0400

    Changed API endpoint in Swagger initializer

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,5 +1,5 @@
-GrapeSwaggerRails.options.app_name     = 'api.gwentcards.com'
+GrapeSwaggerRails.options.app_name     = 'gwentcards.herokuapp.com'
 GrapeSwaggerRails.options.url          = '/v1/swagger_doc'
-GrapeSwaggerRails.options.app_url      = Rails.env.production? ? 'http://gwentcards.rpgcamp.com' : 'http://localhost:3000'
+GrapeSwaggerRails.options.app_url      = Rails.env.production? ? 'http://gwentcards.herokuapp.com' : 'http://localhost:3000'
 GrapeSwaggerRails.options.api_key_name = 'access_token'
 GrapeSwaggerRails.options.api_key_type = 'query'
